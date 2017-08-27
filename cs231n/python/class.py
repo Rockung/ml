7b07d52f6c20d9dee72724bc20f0986897ba597a
xiwen, peng <xiwen.peng@lesiji.top>
2017-08-27 21:32:33 +0800

    class in python

---
+++ cs231n/python/class.py
@@ -0,0 +1,13 @@
+class Greeter(object):
+  def __init__(self, name):
+    self.name = name
+    
+  def greet(self, loud = False):
+    if loud:
+      print('Hello, %s' % self.name.upper())
+    else:
+      print('Hello, %s' % self.name)
+      
+g = Greeter('Fred')
+g.greet()
+g.greet(loud=True)
